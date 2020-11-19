7d766d740f960e385e41af5ee955c2d3bb235f31
Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>
2020-11-19 19:11:13 +0200

    Generics

--- src/main/java/com/lseg/generics/GenericAlgorithms.java
+++ src/main/java/com/lseg/generics/GenericAlgorithms.java
@@ -1,6 +1,7 @@
 package com.lseg.generics;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class GenericAlgorithms {
 
@@ -9,12 +10,26 @@ public class GenericAlgorithms {
         Integer[] arr = {1, 2, 3, 8, 9};
         swap(arr, 0, 4);
         System.out.println(Arrays.toString(arr));
-        System.out.println(arr);
+
+        int max = maxFromRange(List.of(arr), 2, 4);
+        System.out.println("max = " + max); //8 dupa swap
     }
 
     private static <T> void swap(T[] arr, int initial, int other) {
         T temp = arr[initial];
         arr[initial] = arr[other];
         arr[other] = temp;
+    }
+
+    private static <T extends Comparable<T>> T maxFromRange(List<T> list, int start, int end) {
+
+        T maxElem = list.get(start);
+
+        for (++start; start < end; ++start) {
+            if (maxElem.compareTo(list.get(start)) < 0) {
+                maxElem = list.get(start);
+            }
+        }
+        return maxElem;
     }
 }
